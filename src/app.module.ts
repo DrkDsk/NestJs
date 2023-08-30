359695c1a76781bf7bfdfcc1b5943cafecb208ef
Alfredo Palacios <joseaph.1998@gmail.com>
2023-08-29 19:54:06 -0600

    modulo de usuarios creado e implementación de mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ItemsModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
